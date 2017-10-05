20080dadd34afd48fbab3c009c69498a339a5c4f
Tim Fogarty <timothy.fogo@gmail.com>
2017-10-05 11:36:53 -0700

    Refactor app

--- app.js
+++ app.js
@@ -4,27 +4,28 @@ const app = express()
 
 app.use(express.static('dist'))
 
+var petflix;
+
 mongodb.MongoClient.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/test').then(db => {
-  
-  let petflix = db.collection('petflix')
+  petflix = db.collection('petflix')
+})
 
-  app.get('/videos', (req, res) => {
-    petflix.find().toArray().then(data => {
-      res.send(data)
-    })
+app.get('/videos', (req, res) => {
+  petflix.find().toArray().then(data => {
+    res.send(data)
   })
-  
-  app.get('/random', (req, res) => {
-    petflix.aggregate([{ $sample: { size: 1 } }]).toArray().then(data => {
-      res.redirect(`/view/${data[0]._id}`)
-    })
-  })
-  
-  app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-  })
+})
 
-  app.listen(process.env.PORT || 8000, () => {
-    console.log('listening')
+app.get('/random', (req, res) => {
+  petflix.aggregate([{ $sample: { size: 1 } }]).toArray().then(data => {
+    res.redirect(`/view/${data[0]._id}`)
   })
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log('listening on port 8000')
 })
--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,4 +1,4 @@
-import * as types from '../constants/ActionTypes'
+import * as types from '../constants/actionTypes'
 
 const reducer = (state, action) => {
   switch (action.type) {
